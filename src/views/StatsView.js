76bcf4cbe530d11ae6462b4fdc0b09a6406b3b1b
Nicolas Veys <nicolas.veys@student.howest.be>
2022-03-21 12:52:47 +0100

    Add chart.js library, create chart for macros

--- src/views/StatsView.js
+++ src/views/StatsView.js
@@ -1,14 +1,75 @@
+import {
+  CARBS_PERCENTAGE,
+  PROTEINS_PERCENTAGE,
+  FATS_PERCENTAGE,
+} from '../config';
+import { Chart, registerables } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(...registerables, ChartDataLabels);
+
 class StatsView {
   #parentElement = document.querySelector('#person-data');
+  #ctx;
 
   displayStats(data) {
     this.#clear();
     const markup = this.#generateMarkup(data);
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    this.#displayChart(data.macros);
   }
 
   #clear() {
     this.#parentElement.innerHTML = '';
+  }
+
+  #displayChart(macros) {
+    this.#ctx = document.querySelector('#macroChart');
+    new Chart(this.#ctx, {
+      type: 'pie',
+      data: {
+        labels: [
+          `Carbs ${CARBS_PERCENTAGE}%`,
+          `Proteins ${PROTEINS_PERCENTAGE}%`,
+          `Fats ${FATS_PERCENTAGE}%`,
+        ],
+        datasets: [
+          {
+            label: 'macros',
+            data: [macros.carbs, macros.proteins, macros.fats],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          title: { text: 'Macros', display: true },
+          datalabels: {
+            formatter: function (value) {
+              return `${value} grams`;
+            },
+          },
+          tooltip: {
+            callbacks: {
+              label: function (tooltipItem) {
+                return `${
+                  tooltipItem.label
+                }: ${tooltipItem.raw.toString()} grams`;
+              },
+            },
+          },
+        },
+      },
+    });
   }
 
   #generateMarkup(data) {
@@ -27,9 +88,7 @@ class StatsView {
     </div>
     <div>
         <h3>Macros</h3>
-        <p>Carbs: ${data.macros.carbs} grams</p>
-        <p>Protein: ${data.macros.proteins} grams</p>
-        <p>Fat: ${data.macros.fats} grams</p>
+        <canvas id="macroChart" width="400" height="400"></canvas>
     </div>
 `;
   }
